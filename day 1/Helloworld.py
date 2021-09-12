732f5ddc584db8c4d9d47ce119defdb7d1e93a76
Mau5trakt <kubzalemany@hotmail.com>
2021-09-11 21:34:35 -0600

    Exercise 2 Day 1

---
+++ day 1/Helloworld.py
@@ -0,0 +1,28 @@
+#Open the python interactive shell and do the following operations. The operands are 3 and 4.
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+"""
+Write strings on the python interactive shell. The strings are the following:
+Your name
+Your family name
+Your country
+I am enjoying 30 days of python
+"""
+
+print("My Name is Enrique")
+print("My family name is Alemany")
+print("Nicaragua")
+print("I am enjoying 30 days of python")
+#Check the data types of the following data:
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))

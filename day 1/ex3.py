850975d6945bb69125ced735f57f19a3631f07de
Mau5trakt <kubzalemany@hotmail.com>
2021-09-11 21:35:13 -0600

    Exercise 3 Day 1: Find the Euclidean distance between two given ponints

---
+++ day 1/ex3.py
@@ -0,0 +1,12 @@
+#Find an Euclidian distance between (2, 3) and (10, 8)
+
+x1 = 2
+y1 = 3
+
+x2 = 10
+y2 = 8
+
+EuDistance1 =   ((y2-y1)**2) + ((x2-x1)**2)
+EuDistance2 = EuDistance1**0.5
+
+print(f"The Euclidian Distance between (2, 3) and (10, 8) is: {EuDistance2}")
